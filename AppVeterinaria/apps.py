199a55889bbaa5780b30218d0a76a97ea8fcd5ab
nico1018 <roscianinicolas99@gmail.com>
2023-10-16 13:19:31 -0300

    actualizando el commit

--- AppVeterinaria/apps.py
+++ AppVeterinaria/apps.py
@@ -1,6 +1,6 @@
 from django.apps import AppConfig
 
 
-class ApprecetasConfig(AppConfig):
+class AppveterinariaConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
-    name = 'AppRecetas'
+    name = 'AppVeterinaria'
